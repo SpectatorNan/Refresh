e06290931ca60d614dc639c971be6324d577e972
spectatorMrZ <zhuchunan001@sina.com>
2021-03-09 00:21:08 +0800

    update platforms

--- Package.swift
+++ Package.swift
@@ -5,6 +5,12 @@ import PackageDescription
 
 let package = Package(
     name: "Refresh",
+    platforms: [
+        .iOS(.v13),
+        .macOS(.v10_15),
+        .tvOS(.v13),
+        .watchOS(.v6)
+    ],
     products: [
         .library(
             name: "Refresh",
